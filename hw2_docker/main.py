3251533379d2ab1eadf9d801ff51975d379d69e8
nikklaud <titnik47@gmail.com>
2024-10-26 12:37:22 +0500

    fix bug with docker

--- hw2_docker/main.py
+++ hw2_docker/main.py
@@ -1,1 +1,10 @@
-print("Мой первый контейнер в жизни")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello docker"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
